badbb57384b9a48c88f8e8fbe093a2569484ad20
KirraKotsenburg <kirrakotz@hotmail.com>
2024-04-17 01:25:35 -0600

    added 4bit squarer and multiplier

--- 4b_MMult.v
+++ 4b_MMult.v
@@ -1,3 +1,8 @@
+/*********************
+4-bit Mastrovito Multiplier
+Takes in two 4-bit inputs, multiplies
+them, and outputs the product as 4-bit Z
+*********************/
 module 4b_MMult(
     input [3:0]A,
     input [3:0] B,
@@ -8,9 +13,21 @@ module 4b_MMult(
 reg [6:0] S;
 
 always@(*) begin
-    
-    
+    	// Multiply A and B
+	S[0] = A[0] & B[0];
+	S[1] = (A[1] & B[0]) ^ (A[0] & B[1]);
+	S[2] = (A[2] & B[0]) ^ (A[1] & B[1]) ^ (A[0] & B[2]);
+	S[3] = (A[2] & B[1]) ^ (A[1] & B[2]) ^ (A[3] & B[0]) ^ (A[0] & B[3]);
+	S[4] = (A[2] & B[2]) ^ (A[3] & B[1]) ^ (A[1] & B[3]);
+	S[5] = (A[3] & B[2]) ^ (A[2] & B[3]);
+    S[0] = A[3] & B[3];
 
+	
+	//Modulo operation With P(x) = x^4 + x^3 + 1
+	Z[0] = S[0] ^ S[4] ^ S[5] ^ S[6];
+	Z[1] = S[1] ^ S[5] ^ S[6];
+	Z[2] = S[2] ^ S[6];
+    Z[3] = S[3] ^ S[4] ^ S[5] ^ S[6];
 
 end
 
---
+++ 4b_SQR.v
@@ -0,0 +1,33 @@
+/*********************
+4-bit Mastrovito Multiplier modified for squaring operation
+Takes in one 4-bit input, squares
+it, and outputs the product as 4-bit Z
+*********************/
+module 4b_SQR(
+    input [3:0]A,
+    output reg[3:0] Z
+    );
+
+// Temporary reg
+reg [6:0] S;
+
+always@(*) begin
+    	// Square A
+	S[0] = A[0] & A[0];
+	S[1] = (A[1] & A[0]) ^ (A[0] & A[1]);
+	S[2] = (A[2] & A[0]) ^ (A[1] & A[1]) ^ (A[0] & A[2]);
+	S[3] = (A[2] & A[1]) ^ (A[1] & A[2]) ^ (A[3] & A[0]) ^ (A[0] & A[3]);
+	S[4] = (A[2] & A[2]) ^ (A[3] & A[1]) ^ (A[1] & A[3]);
+	S[5] = (A[3] & A[2]) ^ (A[2] & A[3]);
+    S[0] = A[3] & A[3];
+
+	
+	//Modulo operation With P(x) = x^4 + x^3 + 1
+	Z[0] = S[0] ^ S[4] ^ S[5] ^ S[6];
+	Z[1] = S[1] ^ S[5] ^ S[6];
+	Z[2] = S[2] ^ S[6];
+    Z[3] = S[3] ^ S[4] ^ S[5] ^ S[6];
+
+end
+
+endmodule 
